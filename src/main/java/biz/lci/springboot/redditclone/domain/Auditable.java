5fe16e04509563a6c884036e78e76c1442357cb3
tlanders <tlanders@users.noreply.github.com>
2019-03-03 15:39:48 -0600

    added abstract class Auditable and extended it

---
+++ src/main/java/biz/lci/springboot/redditclone/domain/Auditable.java
@@ -0,0 +1,24 @@
+package biz.lci.springboot.redditclone.domain;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+    @CreatedDate
+    private LocalDateTime creationDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+    @CreatedBy
+    private String createdBy;
+    @LastModifiedBy
+    private String lastModifiedBy;
+}
