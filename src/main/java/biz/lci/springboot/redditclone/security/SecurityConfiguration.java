6907c52b66febe04ac98932d8d40b7d861aabb59
tlanders <768713+tlanders@users.noreply.github.com>
2019-03-10 17:28:05 -0500

    secured actuator endpoints

--- src/main/java/biz/lci/springboot/redditclone/security/SecurityConfiguration.java
+++ src/main/java/biz/lci/springboot/redditclone/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package biz.lci.springboot.redditclone.security;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +20,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+                .requestMatchers(EndpointRequest.to("info")).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+                .antMatchers("/actuator/").hasRole("ADMIN")
                 .antMatchers("/").permitAll()
                 .antMatchers("/link/submit").hasRole("USER")
             .and()
