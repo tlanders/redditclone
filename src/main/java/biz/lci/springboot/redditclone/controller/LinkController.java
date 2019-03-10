17b662e89938d5f14ad57bf0bea6093a7a692783
tlanders <tlanders@users.noreply.github.com>
2019-03-09 23:11:38 -0600

    added submit link

--- src/main/java/biz/lci/springboot/redditclone/controller/LinkController.java
+++ src/main/java/biz/lci/springboot/redditclone/controller/LinkController.java
@@ -2,10 +2,15 @@ package biz.lci.springboot.redditclone.controller;
 
 import biz.lci.springboot.redditclone.domain.Link;
 import biz.lci.springboot.redditclone.repository.LinkRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 //@RestController
@@ -13,6 +18,8 @@ import java.util.Optional;
 //@RequestMapping("/links")
 public class LinkController {
     private LinkRepository linkRepository;
+
+    private Logger logger = LoggerFactory.getLogger(LinkController.class);
 
     public LinkController(LinkRepository linkRepository) {
         this.linkRepository = linkRepository;
@@ -23,6 +30,29 @@ public class LinkController {
 //        model.addAttribute("pageTitle", "This is page foo");
 //        return "foo";
 //    }
+
+    @GetMapping("/link/submit")
+    public String newLinkForm(Model model) {
+        model.addAttribute("link",new Link());
+        return "link/submit";
+    }
+
+    @PostMapping("/link/submit")
+    public String createLink(@Valid Link link, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if(bindingResult.hasErrors()) {
+            logger.debug("validation errors found when submitting a link");
+            model.addAttribute("link", link);
+            return "link/submit";
+        } else {
+            // save link
+            linkRepository.save(link);
+            logger.info("link was saved successfully");
+            redirectAttributes
+                    .addAttribute("id", link.getId())
+                    .addFlashAttribute("success", true);
+            return "redirect:/link/{id}";
+        }
+    }
 
     // list
     @GetMapping("/")
@@ -42,6 +72,7 @@ public class LinkController {
         Optional<Link> link = linkRepository.findById(id);
         if(link.isPresent()) {
             model.addAttribute("link", link.get());
+            model.addAttribute("success", model.containsAttribute("success"));
             return "link/view";
         } else {
             return "redirect:/";
--- src/main/java/biz/lci/springboot/redditclone/domain/Link.java
+++ src/main/java/biz/lci/springboot/redditclone/domain/Link.java
@@ -2,12 +2,14 @@ package biz.lci.springboot.redditclone.domain;
 
 import biz.lci.springboot.redditclone.service.BeanUtil;
 import lombok.*;
+import org.hibernate.validator.constraints.URL;
 import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.LocalDateTime;
@@ -25,9 +27,14 @@ public class Link extends Auditable {
     @Id
     @GeneratedValue
     private Long id;
+
     @NonNull
+    @NotEmpty(message = "Please enter a title.")
     private String title;
+
     @NonNull
+    @NotEmpty(message = "Please enter a URL.")
+    @URL(message = "Please enter a valid URL.")
     private String url;
 
     @OneToMany(mappedBy = "link")
