8a68a6f57cb9cf3eb5d611a942adad5a34922c24
tlanders <tlanders@users.noreply.github.com>
2019-03-15 13:48:39 -0500

    showing comments on view page

--- src/main/java/biz/lci/springboot/redditclone/domain/Comment.java
+++ src/main/java/biz/lci/springboot/redditclone/domain/Comment.java
@@ -1,11 +1,16 @@
 package biz.lci.springboot.redditclone.domain;
 
+import biz.lci.springboot.redditclone.service.BeanUtil;
 import lombok.*;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Entity
 @NoArgsConstructor
@@ -22,4 +27,12 @@ public class Comment extends Auditable {
     @NonNull
     @ManyToOne
     private Link link;
+
+    public String getPrettyTime() {
+        return BeanUtil.getBean(PrettyTime.class).format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    protected Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
 }
